8aa6a7d314057583e0679931d32fae0780d3cada
Otho Silva Costa <othosilco@gmail.com>
2024-04-15 22:26:40 -0300

    correção bg

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -8,19 +8,19 @@ import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
     return (
-        <Navbar bg="dark" expand="lg">
+        <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="" className="brand-logo" text-light >ESG.web</Navbar.Brand>
+                <Navbar.Brand href="" className="brand-logo" >ESG.web</Navbar.Brand>
 
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} className="nav-Item" style={{ color: "#ffffff" }} to="/">
+                        <Nav.Link as={Link} className="nav-Item" to="/">
                             Início
                         </Nav.Link>
 
-                        <Nav.Link as={Link} className="nav-Item" style={{ color: "#ffffff" }} to="/tarefas">
+                        <Nav.Link as={Link} className="nav-Item" to="/tarefas">
                             Iniciativas
                         </Nav.Link>
                     </Nav>
